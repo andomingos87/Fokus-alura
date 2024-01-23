7f1f0e9af8ba0f9c441ddadd28ddf8ee7c193e08
Anderson Domingos <andomingos87@gmail.omc>
2024-01-22 22:56:28 -0300

    update

--- script-crud.js
+++ script-crud.js
@@ -1,6 +1,18 @@
 const botaoAddTarefa = document.querySelector('.app__button--add-task')
 const formAddTarefa = document.querySelector('.app__form-add-task')
+const textArea = document.querySelector('.app__form-textarea')
+
+const tarefas = []
 
 botaoAddTarefa.addEventListener('click', () => {
     formAddTarefa.classList.toggle('hidden')
+})
+
+formAddTarefa.addEventListener('submit', (evento) => {
+    evento.preventDefault()
+    const tarefa = {
+        descricao: textArea.value
+    }
+    tarefas.push(tarefa)
+    localStorage.setItem('tarefas', JSON.stringify(tarefas))
 })
