49b9811edf83855f149044a27eed09420b86ad11
Anderson Domingos <andomingos87@gmail.com>
2024-01-29 09:05:15 -0300

    update

--- script-crud.js
+++ script-crud.js
@@ -1,8 +1,40 @@
 const botaoAddTarefa = document.querySelector('.app__button--add-task')
 const formAddTarefa = document.querySelector('.app__form-add-task')
 const textArea = document.querySelector('.app__form-textarea')
+const ulTarefas = document.querySelector('.app__section-task-list')
 
-const tarefas = []
+const tarefas = JSON.parse(localStorage.getItem('tarefas')) || []
+
+function criarElementoTarefa(tarefa) {
+    const li = document.createElement('li')
+    li.classList.add('app__section-task-list-item')
+
+    const svg = document.createElement('svg')
+    svg.innerHTML = `
+    <svg class="app_section-task-icon-status" width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+    <circle cx="12" cy="12" r="12" fill="#FFF"></circle>
+    <path d="M9 16.1719L19.5938 5.57812L21 6.98438L9 18.9844L3.42188 13.4062L4.82812 12L9 16.1719Z" fill="#01080E"></path>
+</svg>
+    `
+    svg.classList.add('app__section-task-icon-status')
+
+    const p = document.createElement('p')
+    p.classList.add('app__section-task-list-item-description')
+    p.textContent = tarefa.descricao
+
+    const button = document.createElement('button')
+    button.classList.add('app__button-edit')
+
+    const imgBotao = document.createElement('img')
+    img.setAttribute('src', '/imagens/edit.png')
+    button.append(imgBotao)
+
+    li.append(svg)
+    li.append(p)
+    li.append(button)
+
+    return li
+}
 
 botaoAddTarefa.addEventListener('click', () => {
     formAddTarefa.classList.toggle('hidden')
@@ -16,3 +48,8 @@ formAddTarefa.addEventListener('submit', (evento) => {
     tarefas.push(tarefa)
     localStorage.setItem('tarefas', JSON.stringify(tarefas))
 })
+
+tarefas.forEach(tarefa => {
+    const elementoTarefa = criarElementoTarefa(tarefa)
+    ulTarefas.append(elementoTarefa)
+});
